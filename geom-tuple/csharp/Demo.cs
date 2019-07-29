10231016b4adcdc277ed70070b1032169c1ad28b
Meng <gaomeng1900@gmail.com>
2019-07-29 23:04:17 +0800

    geomtuple C# demo

---
+++ geom-tuple/csharp/Demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Ali.GeomTuple.V0;
+
+namespace geom_tuple
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            byte[] buffer = File.ReadAllBytes("./test.geomtuple.dimbin");
+            GeomData geom = GeomTuple.Parse(buffer);
+            Console.WriteLine(geom);
+            AttributeData attr;
+            geom.attributes.TryGetValue("position", out attr);
+            Console.WriteLine(attr);
+            Console.WriteLine(geom.index);
+        }
+    }
+}
